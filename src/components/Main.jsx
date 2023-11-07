5521a67d419ffcd0e01881b5aa330a3f638e607c
vinuvarsanth <vinuvarsanth01@gmail.com>
2023-11-07 16:24:56 +0530

    added background color

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 
 const Main = () => {
+  const styling = {
+    textAlign: "center",
+    position: "fixed",
+    middle: 20,
+    left: 20,
+    right: 20,
+    padding: "10px",
+  };
   return (
-    <form>
+    <form style={styling}>
       <div>
-        <label htmlFor="username">Username : </label>
-        <input type="text" id="username" name="username" />
+        <p>
+          <label htmlFor="FirstName">FirstName : </label>
+          <input type="text" id="FirstName" name="FirstName" />
+        </p>
       </div>
       <div>
-        <label htmlFor="password">Password : </label>
-        <input type="password" id="password" name="password" />
+        <p>
+          <label htmlFor="LastName">LastName : </label>
+          <input type="text" id="LastName" name="LastName" />
+        </p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="username">Username : </label>
+          <input type="text" id="username" name="username" />
+        </p>
+      </div>
+      <div>
+        <p>
+          <label htmlFor="password">Password : </label>
+          <input type="password" id="password" name="password" />
+        </p>
       </div>
       <button type="submit">Submit</button>
+      <button type="Clear">Clear</button>
     </form>
   );
 };
